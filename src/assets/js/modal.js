0e8a4472b253f13d607152ab1db1a4ccc437adcf
VavraLuka <92930090+VavraLuka@users.noreply.github.com>
2021-12-16 13:59:48 +0100

    Add files via upload

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,1 +1,30 @@
-modal
+var registracijaModal = document.getElementById("registracija-modal");
+var prijavaModal = document.getElementById("prijava-modal");
+
+var openRegistracija = document.getElementById("open-registracija");
+var openPrijava = document.getElementById("open-prijava");
+
+var span = document.getElementsByClassName("close")[0];
+
+openRegistracija.onclick = function() {
+    registracijaModal.style.display = "block";
+}
+openPrijava.onclick = function(){
+    prijavaModal.style.display= "block";
+}
+
+span.onclick = function() {
+    registracijaModal.style.display = "none";
+    prijavaModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == registracijaModal) {
+        registracijaModal.style.display = "none";
+    }
+}
+window.onclick = function(event) {
+    if (event.target == prijavaModal) {
+        prijavaModal.style.display = "none";
+    }
+}
