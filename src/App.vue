8a0356cc5d71a65ecbf071b4ab5d62605f212159
VavraLuka <luka123vavra@gmail.com>
2021-12-16 14:24:47 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <app-navigacija>
+      <Navigacija/>
+    </app-navigacija>
+    <app-header>
+      <Header/>
+    </app-header>
+    <app-registracija>
+      <Registracija/>
+    </app-registracija>
+    <app-prijava>
+      <Prijava/>
+    </app-prijava>
+    <app-katalog>
+      <Katalog/>
+    </app-katalog>
+    <app-oglas>
+      <Oglas/>
+    </app-oglas>
+    <app-moj-profil>
+      <MojProfil/>
+    </app-moj-profil>
+    <app-dodaj-uredaj>
+      <DodajUredaj/>
+    </app-dodaj-uredaj>
+    <app-footer>
+      <Footer/>
+    </app-footer>
   </div>
 </template>
 
+<script>
+import Navigacija from './components/Navigacija.vue'
+import Header from './components/Header.vue'
+import Katalog from './components/Katalog.vue'
+import Footer from './components/Footer.vue'
+import DodajUredaj from './components/DodajUredaj.vue'
+import MojProfil from './components/MojProfil.vue'
+import Registracija from './components/Registracija.vue'
+import Prijava from './components/Prijava.vue'
+import Oglas from './components/Oglas.vue'
+export default {
+  name: 'App',
+  components: {
+    'app-navigacija': Navigacija,
+    'app-header': Header,
+    'app-katalog': Katalog,
+    'app-footer': Footer,
+    'app-dodaj-uredaj': DodajUredaj,
+    'app-moj-profil': MojProfil,
+    'app-registracija': Registracija,
+    'app-prijava': Prijava,
+    'app-oglas': Oglas
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+    font-family: 'Montserrat';
 }
-
-#nav {
-  padding: 30px;
+::-webkit-scrollbar {
+    display: none;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+html{
+  scroll-behavior: smooth;
 }
 </style>
